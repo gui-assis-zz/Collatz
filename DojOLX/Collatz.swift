7031f6d2054775f850a04cd8c24c3dba9789448d
Guilherme Assis <guilherme.assis@olxbr.com>
2017-02-12 17:30:27 -0200

    DojOLX

---
+++ DojOLX/Collatz.swift
@@ -0,0 +1,19 @@
+//
+//  Collatz.swift
+//  DojOLX
+//
+//  Created by Guilherme Assis on 12/02/17.
+//  Copyright © 2017 Guilherme Assis. All rights reserved.
+//
+
+import UIKit
+
+class Collatz {
+    static func collatz(n: Int) -> [Int] {
+        return []
+    }
+    
+    static func sequence(number: Int) -> String {
+        return "O número com a maior sequência entre 1 e \(number) é xxxx e a maior sequência tem xxxx números"
+    }
+}
--- DojOLX/ViewController.swift
+++ DojOLX/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var edtNumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func btnShowResultTapped(_ sender: AnyObject) {
+        let number = Int(edtNumber.text!)
+        lblResult.text = Collatz.sequence(number: number!)
+        lblResult.isHidden = false
+    }
 }
 
